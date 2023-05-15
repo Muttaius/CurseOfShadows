6dd303db8a87a27c105f6ea5e3eb0e84dd02b157
Muttaius <keithdmorrison1@gmail.com>
2023-05-15 15:06:08 +0100

    Adjusted health script and stairs physics

--- CurseOfShadows/Assets/Scripts/PlayerHealth.cs
+++ CurseOfShadows/Assets/Scripts/PlayerHealth.cs
@@ -22,24 +22,18 @@ public class PlayerHealth : MonoBehaviour
 
     void Awake()
     {
-        // Initialise our current health to be equal to our 
-        // starting health at the beginning of the game
+         // starting health at the beginning of the game
         currentHealth = startingHealth;
     }
 
-    // This function is NOT built in to Unity
-    // It will only be called manually by our own code
-    // It must be marked "public" so our other scripts can access it
-    // This function will change the health value of the player
+    
+    // Function will change the health value of the player
     public void ChangeHealth(int changeAmount)
     {
         // Take our current health, add the change amount, and store the result back in the current health variable
         currentHealth = currentHealth + changeAmount;
 
-        // We don't want our current health to go below zero
-        // And we don't want it to go above our starting health
-        // So we use the special "Clamp" function to keep it 
-        // between 0 and our starting health
+        // Prevents health going below 0 or above 100
         currentHealth = Mathf.Clamp(currentHealth, 0, startingHealth);
 
 
@@ -74,15 +68,13 @@ public class PlayerHealth : MonoBehaviour
         SceneManager.LoadScene(targetScene);
     }
 
-    // This simple function will let other scripts ask this one what the current health is
-    // the function RETURNS an integer, meaning it gives a number back to the code that called it
+    // Allows other scripts ask this one what the current health is
     public int GetHealth()
     {
         return currentHealth;
     }
 
-    // This simple function will let other scripts ask this one what the max health is
-    // the function RETURNS an integer, meaning it gives a number back to the code that called it
+    // Allows other scripts to know what the max health is
     public int GetMaxHealth()
     {
         return startingHealth;
