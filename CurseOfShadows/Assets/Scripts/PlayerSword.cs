bc5841661f8dac93a2b32e6fb8f43265223906c9
Muttaius <keithdmorrison1@gmail.com>
2023-05-29 15:16:31 +0100

    Enemy prefabs added for testing

--- CurseOfShadows/Assets/Scripts/PlayerSword.cs
+++ CurseOfShadows/Assets/Scripts/PlayerSword.cs
@@ -9,36 +9,51 @@ public class PlayerSword : MonoBehaviour
     //Unity editor variables
     public GameObject swordPerfab;
     public Vector2 projectileVelocity; //made public to change as and when required.
-    public Vector2 offset;/// public variable to be added for offset 
+    public Vector3 offset;/// public variable to be added for offset 
+    public float cooldown = 3; //number of seconds attack projectile lasts
+
+    private float startTime; //timestamp that timer begins
+
+
+    private void Update()
+    {
+        if (Time.time >= startTime + cooldown)
+        {
+            SwordAttack();
+        }
+    }
+
 
     //Action - fire sword projectile
     public void SwordAttack()
     {
-        //clone projectile
-        GameObject clonedProjectile;
-        
-        //Command to clone projectile and keep result in variable
-        clonedProjectile = Instantiate(swordPerfab);
+            //clone projectile
+            GameObject clonedProjectile;
 
-        //position projectile on player
-        clonedProjectile.transform.position = transform.position;
+            //Command to clone projectile and keep result in variable
+            clonedProjectile = Instantiate(swordPerfab);
 
-        //Direction of attack - always to the right
-        Rigidbody2D projectileRigidbody;
-        
-        //Get rigid body from the cloned object
-        projectileRigidbody = clonedProjectile.GetComponent<Rigidbody2D>();
-        
-        //sets velocity on rigidbody
-        projectileRigidbody.velocity = projectileVelocity;
+            //position projectile on player
+            clonedProjectile.transform.position = transform.position + offset;
 
-        //Play MCSword attack animation
-        Animator playerAnimator;
+            //Direction of attack - always to the right
+            Rigidbody2D projectileRigidbody;
 
-        //get animation already attached
-        playerAnimator = GetComponent<Animator>();
-        playerAnimator.SetTrigger("attack");
+            //Get rigid body from the cloned object
+            projectileRigidbody = clonedProjectile.GetComponent<Rigidbody2D>();
+
+            //sets velocity on rigidbody
+            projectileRigidbody.velocity = projectileVelocity;
+
+            //Play MCSword attack animation
+            Animator playerAnimator;
+
+            //get animation already attached
+            playerAnimator = GetComponent<Animator>();
+            playerAnimator.SetTrigger("attack");
     }
+            
 
+    
 
 }
