04526bf29ffa46684d8fa3c53a48594d2665831b
Muttaius <keithdmorrison1@gmail.com>
2023-06-11 15:52:44 +0100

    All enemy sprites working, transition box added to town to lead to victory screen

---
+++ CurseOfShadows/Assets/Scripts/ScottishWildlifeButton.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScottishWildlifeButton : MonoBehaviour
+{
+
+        public void ScottishWildlife ()
+        {
+            Application.OpenURL("https://scottishwildlifetrust.org.uk/");
+        }
+    
+}
+
