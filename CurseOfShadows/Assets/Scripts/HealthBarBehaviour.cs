cd7dc7a5f49540c3e9a4459b2f08c59bc28f4be7
anus420 <126487119+anus420@users.noreply.github.com>
2023-06-19 15:43:41 +0100

    changes to the health script adding comments

--- CurseOfShadows/Assets/Scripts/HealthBarBehaviour.cs
+++ CurseOfShadows/Assets/Scripts/HealthBarBehaviour.cs
@@ -8,13 +8,19 @@ public class HealthBarBehaviour : MonoBehaviour
     public Vector3 Offset;
 
 
-
+    //geting helth and max health
     public void SetHealth(float health, float maxHealth) 
     { 
+    
+        //checking if the enemies health is lower than its max
         Slider.gameObject.SetActive(health < maxHealth); 
+        //setting the sliders value to the health the enemy has
         Slider.value = health; 
+        //setting the maximim value of the health bar
         Slider.maxValue = maxHealth; 
 
+
+        //using the value collected from health to blend the colour high and colour low and creating a gradient of the 2 colours depending on the state of the health
         Slider.fillRect.GetComponentInChildren<Image>().color = Color.Lerp(low, high, Slider.normalizedValue); 
     } 
 
